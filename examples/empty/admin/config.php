d1c3b14499355937685541a0621ca844b6f2ad5d
Giovanni Sileno <g.sileno@uva.nl>
2012-08-23 17:51:08 +0200

    Moved the examples in another repository

--- examples/empty/admin/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-  // MyM define
-  define('MYM_RELATIVE_PATH', '../../../MyM'); // from admin to mym dir
-  define('CONFIG_RELATIVE_PATH', '../app/config/'); // from admin to config dir
--- examples/empty/admin/index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-require_once('./config.php');
-define("ADMIN_PATH", __DIR__);
-
-require_once(CONFIG_RELATIVE_PATH.'/config.php');
-require_once(MYM_RELATIVE_PATH.'/index.php');
-
-?>
